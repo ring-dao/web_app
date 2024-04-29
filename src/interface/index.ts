a3236237f0aaab7cd84dcb606f6df44cc93ce561
Elli610 <nathan@cypherlab.fr>
2024-04-30 00:53:58 +0200

    fix rebase

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,4 +1,4 @@
-export {ITopic} from './ITopic';
-export {IPostTopic} from './IPostTopic';
-export {IMessage} from './IMessage';
-export {IPostMessage} from './IPostMessage';
+export {type ITopic} from './ITopic';
+export {type IPostTopic} from './IPostTopic';
+export {type IMessage} from './IMessage';
+export {type IPostMessage} from './IPostMessage';
